1cd5f4c10024e0f0a830c99d494b2bc815d75d2e
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2023-08-23 14:46:28 +0300

    Initial commit

--- src/Task1_3_10_1.java
+++ src/Task1_3_10_1.java
@@ -1,5 +1,11 @@
-public class Main {
+public class Task1_3_10_1 {
     public static void main(String[] args) {
+        System.out.println("""
+                Задание:\s
+                1. Напишите сами крестики-нолики, не подглядывая в наш код.
+
+                Решение:\s""");
+
         System.out.println("Hello world!");
     }
 }
